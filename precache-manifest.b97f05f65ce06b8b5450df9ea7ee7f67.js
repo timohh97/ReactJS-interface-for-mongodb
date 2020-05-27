f1ba5f37b2f92ac9f65131a8f25d4c47cf9a9f09
Timo <timo.schessl@gmx.de>
2020-05-27 21:23:04 +0200

    Updates

---
+++ precache-manifest.b97f05f65ce06b8b5450df9ea7ee7f67.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2315a62fe8db4b4ab1fcfee79ffbab07",
+    "url": "/ReactJS-interface-for-mongodb/index.html"
+  },
+  {
+    "revision": "d03341ae483988a34923",
+    "url": "/ReactJS-interface-for-mongodb/static/css/2.deffb49d.chunk.css"
+  },
+  {
+    "revision": "fc7738ab194e322d969e",
+    "url": "/ReactJS-interface-for-mongodb/static/css/main.e48904e5.chunk.css"
+  },
+  {
+    "revision": "d03341ae483988a34923",
+    "url": "/ReactJS-interface-for-mongodb/static/js/2.b42f7959.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ReactJS-interface-for-mongodb/static/js/2.b42f7959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7738ab194e322d969e",
+    "url": "/ReactJS-interface-for-mongodb/static/js/main.31600b49.chunk.js"
+  },
+  {
+    "revision": "c19ffa639fbcd628ea9a",
+    "url": "/ReactJS-interface-for-mongodb/static/js/runtime-main.7811b928.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ReactJS-interface-for-mongodb/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ReactJS-interface-for-mongodb/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ReactJS-interface-for-mongodb/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ReactJS-interface-for-mongodb/static/media/slick.f97e3bbf.svg"
+  }
+]);
